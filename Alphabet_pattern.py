9e25b340d1d54e04d1250f6d5bcb12939892af11
Aheli kar <ahelikar9@gmail.com>
2025-07-31 16:14:06 +0530

    Create Alphabet_pattern.py

---
+++ Alphabet_pattern.py
@@ -0,0 +1,11 @@
+S=ord('A')
+n=int(input("Enter the eno of terms you want")
+for i in range(n):
+  c=chr(S+i)
+   if(i==2):
+     print(i," "*(n-i),i++ end=" ")
+   elif(j==3):
+     print(" "*(n-i),i++,i,end=" ")
+   else:
+      print()
+print()
