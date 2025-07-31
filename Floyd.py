af5ad9e4d842327011832b8a8c4dd8f76c3e525a
Aheli kar <ahelikar9@gmail.com>
2025-07-31 16:18:28 +0530

    Create Floyd.py

---
+++ Floyd.py
@@ -0,0 +1,9 @@
+n=int(input("Enter the no.of terms")
+for i in range(1,i+1):
+   print(" "*(m-i),end=" ")
+   for j in range(1,i+1):
+     print(c,end=" ")
+     c+=1
+      if j<i:
+        print(" ",end=" ")
+   print()
