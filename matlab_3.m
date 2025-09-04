6bc7e8ebb6a19f37f5beb73d7ba206017129aaff
Aheli kar <ahelikar9@gmail.com>
2025-09-04 10:30:40 +0530

    Create matlab_3.m

---
+++ matlab_3.m
@@ -0,0 +1,19 @@
+%{a=[7,8,15,22];
+%{Melem=a.*a;
+%{display(Melem);
+%{S=[10,20,30,40];
+%{S=[2,S];
+%{disp(S);
+%{disp(S./5);
+%{disp(S./S);
+%{disp(S.*S);
+A=[2 4 6;1 3 5;7 9 11];
+B=[1 0 2;3 1 4;5 2 6];
+disp(A+2.*B);
+disp(A-B);
+disp(A.*B);
+disp(A*B);
+disp(A./B);
+
+
+
